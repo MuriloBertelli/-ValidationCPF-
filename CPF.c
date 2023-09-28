75b8e728bba096c691b9886290222a1f34f91195
MuriloBertelli <mrlbertelli@gmail.com>
2023-09-28 16:32:32 -0300

    Update CPF.c

--- CPF.c
+++ CPF.c
@@ -1,68 +1,67 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <string.h>
 
-bool validaCPF(long long cpf);
+int main()
+{
+    
+    char cpf[15];
+    int i, j, soma = 0, resto, digito1, digito2;
 
-int main() {
-    long long cpf;
+    printf("Digite o CPF no formato xxx.xxx.xxx-xx: ");
+    scanf("%s", cpf);
 
-    printf("Digite o seu CPF (sem pontos ou hifens): ");
-    scanf("%lld", &cpf);
+    // Removendo os pontos e o traço do CPF
+    char cpfNumerico[12];
+    int count = 0;
+    for (i = 0; i < strlen(cpf); i++)
+    {
+        if (cpf[i] != '.' && cpf[i] != '-')
+        {
+            cpfNumerico[count++] = cpf[i];
+        }
+    }
+    cpfNumerico[count] = '\0';
 
-    if (validaCPF(cpf)) {
+    // Cálculo do 1o dígito
+    for (i = 0, j = 10; i < 9; i++, j--)
+    {
+        soma += (cpfNumerico[i] - '0') * j;
+    }
+    resto = soma % 11;
+    if (resto < 2)
+    {
+        digito1 = 0;
+    }
+    else
+    {
+        digito1 = 11 - resto;
+    }
+
+    // Cálculo do 2o dígito
+    soma = 0;
+    for (i = 0, j = 11; i < 10; i++, j--)
+    {
+        soma += (i == 9 ? digito1 : (cpfNumerico[i] - '0')) * j;
+    }
+    resto = soma % 11;
+    if (resto < 2)
+    {
+        digito2 = 0;
+    }
+    else
+    {
+        digito2 = 11 - resto;
+    }
+
+    // Verificação
+    if (digito1 == (cpfNumerico[9] - '0') && digito2 == (cpfNumerico[10] - '0'))
+    {
         printf("CPF fornecido é válido.\n");
-    } else {
+    }
+    else
+    {
         printf("CPF fornecido é inválido.\n");
     }
 
     return 0;
-}
-
-bool validaCPF(long long cpf) {
-    int i, j, digito, resto;
-    long long soma, temp, fator;
-
-    // Calcula o primeiro dígito verificador
-    temp = cpf / 100;
-    soma = 0;
-    fator = 10;
-    for (i = 0; i < 9; i++) {
-        digito = temp % 10;
-        soma += digito * fator;
-        fator--;
-        temp /= 10;
-    }
-    resto = soma % 11;
-    if (resto < 2) {
-        if ((cpf % 100) / 10 != 0) {
-            return false;
-        }
-    } else {
-        if ((cpf % 100) / 10 != (11 - resto)) {
-            return false;
-        }
-    }
-
-    // Calcula o segundo dígito verificador
-    temp = cpf / 10;
-    soma = 0;
-    fator = 11;
-    for (j = 0; j < 10; j++) {
-        digito = temp % 10;
-        soma += digito * fator;
-        fator--;
-        temp /= 10;
-    }
-    resto = soma % 11;
-    if (resto < 2) {
-        if (cpf % 10 != 0) {
-            return false;
-        }
-    } else {
-        if (cpf % 10 != (11 - resto)) {
-            return false;
-        }
-    }
-
-    return true;
 }
